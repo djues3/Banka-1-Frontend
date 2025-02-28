9f0aa3095a6aab9068a366efaf2a75a3517bfe53
ZixHD <havos25@gmail.com>
2025-02-28 23:12:17 +0100

    Drawer and navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Login from './pages/Login';
 import { ThemeProvider } from './context/ThemeContext';
 import ThemeToggle from './components/common/ThemeToggle';
+import UserPortal from './pages/UserPortal';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/user-portal" element={<UserPortal />} />
           {/* Add more routes as needed */}
         </Routes>
         <ThemeToggle />
--- src/pages/UserPortal.jsx
+++ src/pages/UserPortal.jsx
@@ -1,11 +1,17 @@
 
+
+import Navbar from "../components/common/Navbar";
+import Sidebar from "../components/common/Sidebar";
+
+
 const UserPortal = () => {
     return (
         <div>
-
+            <Navbar/>
+            <Sidebar/>
         </div>
     )
 }
 
 
-export default UserPortal();
+export default UserPortal;
