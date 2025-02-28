982c086557793261bcb0cfc5497dfb4ed8dc6c79
David Djuretanovic <djues3@gmail.com>
2025-02-28 06:15:38 +0100

    Reenabled auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -9,12 +9,11 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
-    // if (this.authService.isLoggedIn()) {
-    //   return true;
-    // } else {
-    //   this.router.navigate(['/login']);
-    //   return false;
-    // }
-    return true;
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']).then(r => {});
+      return false;
+    }
   }
 }
