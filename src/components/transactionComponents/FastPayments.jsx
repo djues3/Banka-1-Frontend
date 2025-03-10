d7175278433cdd0ce302f2a16783e4178a046eea
David Djuretanovic <djues3@gmail.com>
2025-03-10 04:28:55 +0100

    Removed dead code

--- src/components/transactionComponents/FastPayments.jsx
+++ src/components/transactionComponents/FastPayments.jsx
@@ -1,14 +1,8 @@
 import React, {useEffect, useState} from "react";
-import { Button, Box, Typography} from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import {Box, Button, Typography} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 import AddFastPayment from "./AddFastPayment";
-import {addFastPaymentPerson} from "../../services/TansactionService";
-import {
-    createRecipient,
-    fetchRecipientsForFast,
-    getUserIdFromToken
-} from "../../services/AxiosBanking";
-
+import {createRecipient, fetchRecipientsForFast, getUserIdFromToken} from "../../services/AxiosBanking";
 
 
 const FastPayments = () => {
