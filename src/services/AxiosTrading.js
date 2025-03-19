5828455a85d8e2caca1432432fe31a561c89cbfc
brankadelic <bdelic6721rn@raf.rs>
2025-03-20 00:51:22 +0100

    Dodata dva portala za kupovinu hartija od vrednosti jedan za Aktuara jedan za obicnog korisnika, bez becka, male izmene u AxiosTrading

--- src/services/AxiosTrading.js
+++ src/services/AxiosTrading.js
@@ -1,9 +1,41 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-const apiBanking = axios.create({
-  baseURL: `${process.env.REACT_APP_TRADING_API_URL}`,
+const apiTrading = axios.create({
+  baseURL: "http://localhost:3000/trading",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+
+export const fetchSecurities = async () => {
+
+  try {
+    const response = await apiTrading.get(`/securities`);
+    return response.data;
+  } catch (error) {
+    console.error("Greška pri dohvatanju hartija:", error);
+    throw error;
+  }
+};
+
+export const fetchAvailableSecurities = async () => {
+  try {
+    const response = await apiTrading.get(`/securities/available`);
+    return response.data;
+  } catch (error) {
+    console.error("Greška pri dohvatanju dostupnih hartija:", error);
+    throw error;
+  }
+};
+
+export const updateSecurity = async (ticker, newData) => {
+  try {
+    const response = await apiTrading.put(`/securities/${ticker}`, newData);
+    return response.data;
+  } catch (error) {
+    console.error(`Greška pri ažuriranju hartije ${ticker}:`, error);
+    throw error;
+  }
+};
